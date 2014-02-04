a55b09f285ed718bad19f0fb14699232a3ecaafe
AlexTalker <alextalker@openmailbox.org>
2014-02-04 20:53:42 +0400

    Now every application on one page: /stream/

--- app.rb
+++ app.rb
@@ -21,22 +21,12 @@ end
 # 		"<a href='/stream/1'>Go to first stream page!</a>"
 # 	end
 # end
-
-# def page_messages(n)
-# 	n = n.to_i
-# 	s = String.new
-# 	if (n.integer?) and (n>0) and ($msgs.length >= ((n-1) * 10))
-# 		s << (erb :message, :locals => {:n => n, :page => :message,:title => "Page #{n}"})
-# 	else
-# 		raise 404
-# 	end
-# end
-
 get '/' do
 #  draw a form to input message
-	erb :notification, :locals => {:title => "Home page."} do
-		erb :form
-	end
+# 	erb :notification, :locals => {:title => "Home page."} do
+# 		erb :form
+# 	end
+	redirect to('/stream')
 end
 
 post '/post' do
@@ -45,11 +35,10 @@ post '/post' do
 end
 
 get '/stream/:page' do
-# 	page_messages(params[:page])
+
 	n = params[:page].to_i
-# 	s = String.new
 	if (n.integer?) and (n>0) and ($msgs.length >= ((n-1) * 10))
-		erb :message, :locals => {:n => n, :page => :message,:title => "Page #{n}"}
+		erb :stream, :locals => {:n => n, :title => "Page #{n}"}
 	else
 		halt 404
 	end
